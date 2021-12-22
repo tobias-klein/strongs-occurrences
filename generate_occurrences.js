342a92696959c5f0786035feafaf2ef1c42aa93e
Tobias Klein <contact@tklein.info>
2021-12-22 17:32:41 +0100

    Initial implementation of script

--- generate_occurrences.js
+++ generate_occurrences.js
@@ -16,3 +16,55 @@
    along with strongs-occurrences. See the file LICENSE.
    If not, see <http://www.gnu.org/licenses/>. */
 
+const strongs = require('strongs');
+const nodeSwordInterface = require('node-sword-interface');
+const fs = require('fs');
+
+const nsi = new nodeSwordInterface();
+const DEBUG = false;
+
+async function getNumberOfOccurrences(key) {
+  results = await nsi.getModuleSearchResults('KJV', key, undefined, 'strongsNumber');
+  return results.length;
+}
+
+async function main() {
+  var count = 0;
+
+  var kjvAvailable = nsi.isModuleInUserDir('KJV');
+  if (!kjvAvailable) {
+    console.log("KJV is not available - please install it before using this script."); 
+    process.exit(1);
+  }
+
+  var occurrenceMap = {};
+  var entryCount = Object.entries(strongs).length;
+
+  if (!DEBUG) {
+    console.log(`Generating strongs_occurrences.json for ${entryCount} entries! WARNING: This is a lengthy process.`);
+    console.log('');
+  }
+
+  for (const [key, object] of Object.entries(strongs)) {
+    process.stdout.write('Getting occurrences for ' + key + ': ');
+    var occurrences = await getNumberOfOccurrences(key);
+    console.log(occurrences);
+
+    occurrenceMap[key] = occurrences;
+    count += 1;
+
+    if (DEBUG && count >= 2) {
+      break;
+    }
+  }
+
+  const occurrencesString = JSON.stringify(occurrenceMap, null, 2);
+
+  try {
+    fs.writeFileSync('strongs_occurrences.json', occurrencesString);
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+main();
